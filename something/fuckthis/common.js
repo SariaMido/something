42d77b31a2cbe84dc410285cdcd62e32c99a7d0f
Danial <osama.al-midani@outlook.com>
2021-03-19 20:38:08 +0100

    AddingFeature

--- something/fuckthis/common.js
+++ something/fuckthis/common.js
@@ -7,22 +7,21 @@ $(function () {
     ];
 
     function buildTable() {
-        let html = `<table class="table"><thead><tr>`;
+        let html = `<table class="table" id="youtuberTable"><thead><tr>`;
 
         html += `
-        <th scope="col">Name</th>
-        <th scope="col">section</th>
-        <th scope="col">age</th>
-        <th scope="col">
-            <span id="showForm" class="controlForm">
-                <i class="fas fa-plus-square"></i>
-            </span>
-            <span id="hideForm" class="controlForm" style="display:none">
-            <i class="fas fa-minus-square"></i>
-            </span>
-        </th>
-        </tr></thead><tr>`;
-
+            <th scope="col">Name</th>
+            <th scope="col">section</th>
+            <th scope="col">age</th>
+            <th scope="col">
+                <span id="showForm" class="controlForm">
+                    <i class="fas fa-plus-square"></i>
+                </span>
+                <span id="hideForm" class="controlForm" style="display:none">
+                <i class="fas fa-minus-square"></i>
+                </span>
+            </th>
+            </tr></thead><tr>`;
         for (let i = 0; i < data.length; i++) {
             html += `<td>${data[i].name}</td>`;
             html += `<td>${data[i].section}</td>`;
@@ -45,6 +44,33 @@ $(function () {
             $('#hideForm').hide()
             $('#showForm').show()
         }
+
     });
+
+    $('#youtuber').submit(function (e) {
+        e.preventDefault();
+        let nameInput = $(this).serializeArray()[0].value;
+        let sectionInput = $(this).serializeArray()[1].value;
+        let ageInput = +$(this).serializeArray()[2].value;
+
+        let youtuber = { name: nameInput, section: sectionInput, age: ageInput };
+
+        data.push(youtuber);
+        buildTable(youtuber);
+
+    });
+
+    // function addToTable(newItem) {
+    //     let newElement = document.createElement('tr');
+    //     newElement.className = 'newItem';
+    //     newElement.innerHTML = `
+    //     <td>${newItem.name}</td>
+    //     <td>${newItem.section}</td>
+    //     <td>${newItem.age}</td>
+    //     `;
+    //     document.getElementById('youtuberTable').appendChild(newElement);
+    // }
+
 });
+
 
